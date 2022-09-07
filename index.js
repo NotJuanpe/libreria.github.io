3fa98c42691420358c4db90b4e074593997bf9b7
Juan P Rodriguez <juannpe01@gmail.com>
2022-09-07 20:33:42 -0300

    Po 4.0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let productos = [
                 {id:1,nombre:"IT", categoria:"Terror", precio: 68, imagen:"",resumen:""},
                 {id:1,nombre:"Death Note", categoria:"Otros", precio: 40, imagen:"",resumen:""},
                 {id:1,nombre:"A Dos Metros de Ti", categoria:"Romance", precio: 64, imagen:"",resumen:""},
-                {id:1,nombre:"Squadron", categoria:"SciFi", precio: 130, imagen:"",resumen:""},
+                {id:1,nombre:"Squadron", categoria:"SciFi", precio: 72, imagen:"",resumen:""},
                 ]
 
 class Tienda{
@@ -19,7 +19,7 @@ class Tienda{
             contenedor.style.width = "18rem";
 
             contenedor.innerHTML = `    <img src=${producto.imagen} class="card-img-top" alt="${producto.nombre}>
-                                        <div class="card-body">
+                                        <div class = "card-body">
                                             <h5 class="card-title">${producto.nombre}</h5>
                                             <h6 class="card-title">Categoria: ${producto.categoria}</h6>
                                             <h6 class="card-title">Precio: ${producto.precio}</h6>
@@ -27,9 +27,7 @@ class Tienda{
                                         </div>`
             let padre = document.querySelector(".contenedor");
 
-            
             padre.appendChild(contenedor);
-            console.log(contenedor)
         }
     }
 
@@ -42,14 +40,32 @@ class Tienda{
 
     filtrarCategoria(productos,categorias){
 
-        const filtrado = productos.filter((filtro) => filtro.categoria.includes(categorias))
+        const filtrado = productos.filter((filtro) => filtro.categoria.includes(categorias));
     
         this.vaciarProductos();
         this.agregarProductos(filtrado);
+
+    }
+
+    filtrarPrecio(productos){
+        var minimo = document.querySelector("#min").value;
+        var maximo = document.querySelector("#max").value;
+        var valoresAceptados = /^[0-9]+$/;
+
+        if(minimo == "") minimo = 0;
+        if(maximo == "") maximo = 10000;
+
+        if(maximo.match(valoresAceptados) && minimo.match(valoresAceptados)){
+            const filtrado = productos.filter((filtro) => (filtro.precio <= maximo) && (filtro.precio >= minimo));
+            this.vaciarProductos();
+            this.agregarProductos(filtrado);
+        }
+
 
     }
 }
 
 var tienda = new Tienda();
 tienda.agregarProductos(productos);
+
 
