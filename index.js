10b5476eace3cd1c4c1f5cbbe7e38a4ce0154b56
Juan P Rodriguez <juannpe01@gmail.com>
2022-09-13 22:28:03 -0300

    Interactua con html

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ let productos = [
                 {id:8,nombre:"Squadron", categoria:"SciFi", precio: 72, imagen:"./media/sander.jpg"},
                 ]
 
+let carrito = []
+
 class Tienda{
 
     agregarProductos(productos){
 
         for(const producto of productos){
             let contenedor = document.createElement("div");
-            contenedor.classList.add("card","col-sm-12","animacion");
+            contenedor.classList.add("card","col-sm-12","animacion","producto");
             contenedor.style.width = "18rem";
 
             contenedor.innerHTML = `    <img src=${producto.imagen} class="card-img-top" alt="${producto.nombre}>
                                         <div class = "card-body">
                                             <h5 class="card-title">${producto.nombre}</h5>
                                             <h6 class="card-title">Categoria: ${producto.categoria}</h6>
-                                            <h6 class="card-title">Precio: ${producto.precio}</h6>
+                                            <h6 class="card-title">Precio: <strong>$${producto.precio}</strong></h6>
+                                            <h6 class="card-title id">Id: ${producto.id}</h6>
+                                            <div class="boton_carrito" onclick="tienda.agregadosAlCarrito(${producto.id},productos,carrito)">
+                                                <button type="button" class="btn btn-light">Add</button>
+                                            </div>
                                         </div>`
             let padre = document.querySelector(".contenedor");
 
@@ -46,9 +52,6 @@ class Tienda{
             this.vaciarProductos();
             this.agregarProductos(filtrado);
         }
-
-
-
     }
 
     filtrarPrecio(productos){
@@ -67,9 +70,83 @@ class Tienda{
 
 
     }
+
+    agregadosAlCarrito(id,productos,carrito){
+        const producto = productos.find(producto => producto.id === id);
+        carrito.push(producto);
+        this.agregarCarrito(producto);
+    }
+
+    agregarCarrito(producto){
+
+            let row = document.createElement("div");
+            row.classList.add("row","lista");
+
+            let contenedor1 = document.createElement("div");
+            contenedor1.classList.add("col-6");
+            contenedor1.innerHTML = `<div class="prod_cart">
+                                            <img src=${producto.imagen} alt="${producto.nombre}">
+                                            <h3 class="ml-3 mb-0">${producto.nombre}</h3>
+                                        </div>`
+            
+            let contenedor2 = document.createElement("div");
+            contenedor2.classList.add("col-2");
+            contenedor2.innerHTML=`<div>
+                                        <h3>$${producto.precio}</h3>
+                                    </div>`
+
+            let contenedor3 = document.createElement("div");
+            contenedor3.classList.add("col-3");
+
+            let contenedor4 = document.createElement("div");
+            contenedor4.classList.add("col-1");
+            contenedor4.innerHTML=`<div onclick="tienda.borrarItem(${producto.id},productos,carrito,event)">
+                                        <button type="button" class="btn btn-danger eliminar">X</button>
+                                    </div>`
+            
+            row.appendChild(contenedor1);
+            row.appendChild(contenedor2);
+            row.appendChild(contenedor3);
+            row.appendChild(contenedor4);
+
+            let padre = document.querySelector(".contenedor_carrito");
+            padre.appendChild(row);
+            console.log(carrito)
+        }
+
+        borrarItem(id,productos,carrito,event){
+
+            const producto = productos.find(producto => producto.id === id);
+            carrito.splice(producto,1)
+
+            const botonClickeado = event.target;
+            botonClickeado.closest('.lista').remove()
+            console.log(carrito)
+        }
+
+        precioFinal(productos){
+
+            var precio = 0;
+            
+            for(const producto of productos){
+                precio += producto.precio
+            }
+
+            return precio
+
+            
+        }
+
+        pagar(){
+
+            alert(`Su total es de $${this.precioFinal(carrito)}`)
+            alert("No es necesario que nos diga donde vive ya lo sabemos")
+            
+
+        }
+
+        
 }
 
 var tienda = new Tienda();
 tienda.agregarProductos(productos);
-
-
